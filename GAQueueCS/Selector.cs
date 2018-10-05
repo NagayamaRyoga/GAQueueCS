2422f3de6908046d02116197e88cdc8369c0e8c0
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2018-10-05 22:14:42 +0900

    fix: Fix undeclared type operator.

--- GAQueueCS/Selector.cs
+++ GAQueueCS/Selector.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace GAQueueCS
 {
-	class Selector
+    using Operator = Func<IEnumerable<Individual>, IEnumerable<Individual>>;
+
+    class Selector
 	{
 		Operator Resize(int size)
 		{
