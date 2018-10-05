af2e54ed4ed48f18b018f1049e58f4db8f09e211
Unknown <allium.fistulosum.39@gmail.com>
2018-10-05 21:39:44 +0900

    add : selector

---
+++ GAQueueCS/Selector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAQueueCS
+{
+	class Selector
+	{
+		Operator Resize(int size)
+		{
+			return delegate (IEnumerable<Individual> arg)
+			{
+				return arg.Take(size);
+			};
+		}
+
+		Operator Resize(float rate)
+		{
+			return delegate (IEnumerable<Individual> arg)
+			{
+				return arg.Take((int) Math.Floor(arg.Count() * rate));
+			};
+		}
+
+		Operator Unique()
+		{
+			return delegate (IEnumerable<Individual> arg)
+			{
+				return arg.Distinct();
+			};
+		}
+
+		/*
+		Operator crampCoefficientOfInbreeding(float maxCOI)
+		{
+			return delegate (IEnumerable<Individual> arg)
+			{
+
+			};
+		}
+		*/
+	}
+}
