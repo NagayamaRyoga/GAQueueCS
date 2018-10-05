9a7f434c085dce68cf03f2ba41771b4fc8618151
Unknown <allium.fistulosum.39@gmail.com>
2018-10-05 21:39:03 +0900

    add : comparator

---
+++ GAQueueCS/Comparator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAQueueCS
+{
+	class Comparator
+	{
+		public static int Through(Individual _, Individual __) => 0;
+
+		public static double Fitness(Individual lhs, Individual rhs)
+		{
+			if (!lhs.Fitness.HasValue || !rhs.Fitness.HasValue) return 0;
+			return lhs.Fitness.Value - rhs.Fitness.Value;
+		}
+
+		public static double RawFitness(Individual lhs, Individual rhs)
+		{
+			if (!lhs.RawFitness.HasValue || !rhs.RawFitness.HasValue) return 0;
+			return lhs.RawFitness.Value - rhs.RawFitness.Value;
+		}
+
+		public static double Recentness(Individual lhs, Individual rhs)
+		{
+			return lhs.BirthYear - rhs.BirthYear;
+		}
+	}
+}
