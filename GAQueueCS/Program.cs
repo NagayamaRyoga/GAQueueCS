1a3d5243648e2bdca2bee9154f2a00977961dee6
Unknown <allium.fistulosum.39@gmail.com>
2018-10-09 17:42:01 +0900

    fix : merge 2 ClampCOI into 1

--- GAQueueCS/Program.cs
+++ GAQueueCS/Program.cs
@@ -51,7 +51,7 @@ namespace GAQueueCS
 				return arg
 					.OrderByDescending(indiv => indiv.Fitness)
 					//.ClampMinCoefficientOfInbreeding(minCOI: 0.1, minCount: 2)
-					.ClampMaxCoefficientOfInbreeding(maxCOI: 0.2, minCount: 2)
+					.ClampCoefficientOfInbreeding(max: 0.2, minCount: 2)
 					.TakeAccurately(2)
 					.RememberParents(hoge => hoge.REX(rand, 5));
 			}
--- GAQueueCS/Selector.cs
+++ GAQueueCS/Selector.cs
@@ -24,10 +24,13 @@ namespace GAQueueCS
 			return ans.Take((int) Math.Floor(ans.Count() * rate));
 		}
 
-		public static IEnumerable<Individual> ClampMinCoefficientOfInbreeding(this IEnumerable<Individual> arg, double minCOI, int minCount)
+		public static IEnumerable<Individual> ClampCoefficientOfInbreeding(
+			this IEnumerable<Individual> arg, int minCount, double? min = null, double? max = null)
 		{
+			if (!min.HasValue && !max.HasValue)	return arg;
+
 			var ans = new List<Individual>();
-			int maxDepth = (int) -Math.Ceiling(Math.Log(minCOI / 2) / Math.Log(2));
+			int maxDepth = (int) -Math.Ceiling(Math.Log((min ?? max.Value) / 2) / Math.Log(2));
 
 			foreach (var indiv in arg)
 			{
@@ -35,35 +38,7 @@ namespace GAQueueCS
 				foreach (var i in ans)
 				{
 					var coi = indiv.CalcCoefficientOfInbreeding(i, maxDepth);
-					if (!coi.HasValue || coi <= minCOI)
-					{
-						flag = false;
-						break;
-					}
-				}
-				if (flag) ans.Add(indiv);
-			}
-
-			//Console.WriteLine($"cramp {arg.Count()}=>{ans.Count()}");
-
-			if (ans.Count() < minCount) return arg;
-			//foreach (var i in ans) Console.WriteLine(i);
-			//Console.WriteLine();
-
-			return ans;
-		}
-
-		public static IEnumerable<Individual> ClampMaxCoefficientOfInbreeding(this IEnumerable<Individual> arg, double maxCOI, int minCount)
-		{
-			var ans = new List<Individual>();
-			int maxDepth = (int) -Math.Ceiling(Math.Log(maxCOI / 2) / Math.Log(2));
-
-			foreach (var indiv in arg)
-			{
-				bool flag = true;
-				foreach (var i in ans)
-				{
-					if (indiv.CalcCoefficientOfInbreeding(i, maxDepth) > maxCOI)
+					if (!coi.HasValue || min > coi || coi < max)
 					{
 						flag = false;
 						break;
